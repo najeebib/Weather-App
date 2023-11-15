5af05bac71e9c66ed9494ce5e20e0e452184e1b3
najeebib <najeebibraheem1999@hotmail.com>
2023-11-15 15:14:39 +0200

    added the class, adapter and layout file for the rv items
    // todo: complete the layout design and adapter functionality

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RelativeLayout homeRL;
+    private ProgressBar loadingPB;
+    private TextView cityNameTV,tempTV,conditionTV;
+    private TextInputEditText cityEdt;
+    private ImageView backIV,iconIV,searchIV;
+    private RecyclerView weatherRV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeRL = findViewById(R.id.idRLHome);
+        loadingPB = findViewById(R.id.idLoading);
+        cityNameTV = findViewById(R.id.idTVCityName);
+        tempTV = findViewById(R.id.idtemp);
+        conditionTV = findViewById(R.id.idTVCondition); 
+        cityEdt = findViewById(R.id.idEdtCity);
+        backIV = findViewById(R.id.idBack);
+        iconIV = findViewById(R.id.idIVIcon);
+        searchIV = findViewById(R.id.idSearch);
+        weatherRV = findViewById(R.id.idRVWeather);
+
+
+
     }
 }
