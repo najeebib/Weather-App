480fad105d92e78040e35ca837427562eb8af95f
najeebib <najeebibraheem1999@hotmail.com>
2023-11-17 15:28:38 +0200

    added way to get the entered city coords
    // todo: finsih the function and get weather data from api

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.location.Address;
+import android.location.Geocoder;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText cityEdt;
     private ImageView backIV,iconIV,searchIV;
     private RecyclerView weatherRV;
+    private ArrayList<WeatherRVModal> WeatherList;
+    private WeatherRVAdapter WeatherAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,8 +45,32 @@ public class MainActivity extends AppCompatActivity {
         iconIV = findViewById(R.id.idIVIcon);
         searchIV = findViewById(R.id.idSearch);
         weatherRV = findViewById(R.id.idRVWeather);
+        WeatherList  = new ArrayList<>();
+        WeatherAdapter = new WeatherRVAdapter(this,WeatherList);
+        weatherRV.setAdapter(WeatherAdapter);
 
 
+
+    }
+    private void getWeatherInfo(String cityName)
+    {
+        List<Address> addresses;
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        try{
+            addresses = geocoder.getFromLocationName(cityName, 1);
+            if (addresses != null && !addresses.isEmpty()) {
+                double latitude = addresses.get(0).getLatitude();
+                double longitude = addresses.get(0).getLongitude();
+                double[] coords = new double[]{latitude, longitude};
+                String apiKey = System.getenv("OpenWeatherMapAppID");
+                String request = "https://api.openweathermap.org/data/3.0/onecall?lat="+String.valueOf(coords[0])+ "&lon="+String.valueOf(coords[0])+"&exclude=minutely,alerts&appid="+apiKey+"&units=metric";
+
+
+            }
+        }
+        catch (IOException e){
+            Log.e("GPS error", "Error getting coordinates: " + e.getMessage());
+        }
 
     }
 }
